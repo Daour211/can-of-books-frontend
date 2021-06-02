bf52301662ac60c77a52707eb5cfda16c47cf6a0
Abdelrahman Mohammad Al Daour <Daour.995@outlook.com>
2021-06-02 19:09:21 +0300

    Finished lab14

--- src/components/BestBooks.js
+++ src/components/BestBooks.js
@@ -2,11 +2,12 @@ import React from 'react';
 import axios from 'axios';
 import { withAuth0 } from "@auth0/auth0-react";
 // import Carousel from 'react-bootstrap/Carousel'
-import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/Card'
+import UpdateBook from './UpdateBook';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/Card';
 import AddBook from './BookFormModal';
-import Button from 'react-bootstrap/Button'
-// import { CardDeck } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+
 
 
 class BestBooks extends React.Component {
@@ -14,11 +15,20 @@ class BestBooks extends React.Component {
         super(props);
         this.state = {
             bookData: [],
-            displayModal: false,
+           // server: process.env.REACT_APP_SERVER
 
-            // server: process.env.REACT_APP_SERVER
+            displayModal: false,
+            displayUpdateModal: false,
+
+            index: 0,
+            bookName: '',
+            bookDescription: '',
+            imgURL: '',
+            bookStatus: ''
         }
     }
+    
+    // for rendering all books
     componentDidMount = async () => {
         const { user } = this.props.auth0;
         // console.log('inside function');
@@ -31,6 +41,7 @@ class BestBooks extends React.Component {
         // console.log(this.state.bookData);
     }
 
+    // for deleting book
     deleteBook = async (idx) => {
         let { user } = this.props.auth0;
           user = {email : user.email}
@@ -41,12 +52,15 @@ class BestBooks extends React.Component {
             bookData: deleteBook.data
         })
     }
+
+    // this function to have the data after adding a book
     updateBookData = (e) => {
         this.setState({
             bookData: e
         })
     }
 
+    // this function to show the modal of adding book
     showModal = async (e) => {
 
         this.setState({
@@ -54,13 +68,88 @@ class BestBooks extends React.Component {
         })
 
     }
+     // this function to hide the modal of adding book and updating a book
     hideModal = async (e) => {
 
         this.setState({
-            displayModal: false
+            displayModal: false,
+            displayUpdateModal: false
         })
 
     }
+
+    // function to select the book that we want to update 
+    showUpdatingForm = (idx)=>{
+        let choosenBook = this.state.bookData.filter((val, index) => {
+            return idx === index;
+        })
+        console.log(choosenBook);
+        
+        // this.state.choosenBook.push(choosenBook[0])
+        // console.log(this.state.choosenBook);
+        
+        this.setState({
+            displayUpdateModal: true,
+            bookName: choosenBook[0].name,
+            bookDescription: choosenBook[0].description,
+            imgURL: choosenBook[0].imgURL,
+            bookStatus: choosenBook[0].status,
+            index: idx
+        })
+        console.log(this.state);
+    }
+
+    // for updating the book name
+    updateBookName = (event) => {
+        this.setState({
+            bookName: event.target.value
+        })
+        // console.log(this.state.bookName);
+    }
+    // for updating the book description
+    updateDescription = (event) => {
+        this.setState({
+            bookDescription: event.target.value
+        })
+        // console.log(this.state.bookDescription);
+    }
+    // for updating the book img URL
+    updateImg = (event) => {
+        this.setState({
+            imgURL: event.target.value
+        })
+        // console.log(this.state.imgURL);
+    }
+    // for updating the book status
+    updateStatus = (event) => {
+        this.setState({
+            bookStatus: event.target.value
+        })
+        // console.log(this.state.bookStatus);
+    }
+
+    // for send the request to update the book
+    updatingBook = async (e) => {
+        e.preventDefault()
+        const { user } = this.props.auth0;
+        const sentData = {
+            bookName: this.state.bookName,
+            bookDescription: this.state.bookDescription,
+            imgURL: this.state.imgURL,
+            bookStatus: this.state.bookStatus,
+            email: user.email
+        }
+
+        let updatedBook = await axios.put(`http://localhost:3001/updateBook/${this.state.index}`, sentData)
+        console.log(updatedBook);
+
+        this.setState({
+            bookData: updatedBook.data.books,
+            displayUpdateModal: false
+        })
+    }
+
+    
 
     render() {
         return (
@@ -68,7 +157,26 @@ class BestBooks extends React.Component {
                 <button onClick={this.showModal}> Add Books</button>
 
 
-                <AddBook displayModal={this.state.displayModal} hideModal={this.hideModal} updateBookData={this.updateBookData} />
+                <AddBook 
+                displayModal={this.state.displayModal} 
+                hideModal={this.hideModal} 
+                updateBookData={this.updateBookData} />
+
+                <UpdateBook
+                displayUpdateModal={this.state.displayUpdateModal}
+                hideAddModal={this.hideAddModal}
+
+                bookName={this.state.bookName}
+                bookDescription={this.state.bookDescription}
+                imgURL={this.state.imgURL}
+                bookStatus={this.state.bookStatus}
+
+                updateBookName={this.updateBookName}
+                updateDescription={this.updateDescription}
+                updateImg={this.updateImg}
+                updateStatus={this.updateStatus}
+
+                updatingBook={this.updatingBook} />
 
                 {/* <Carousel> */}
                 {this.state.bookData.length &&
@@ -87,6 +195,7 @@ class BestBooks extends React.Component {
 
                                         </Card.Text>
                                         <Button onClick={() => this.deleteBook(idx)} variant="primary">Delete Book</Button>
+                                        <Button onClick={() => this.showUpdatingForm(idx)} variant="primary">Update Book</Button>
                                     </Card.Body>
                                 </Card>
                             </CardGroup>
